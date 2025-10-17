2c693aef95675202d9477c8c19b47324ca9153ca
vitorcoelhof <vitorcoelhof@gmail.com>
2025-10-17 15:35:45 -0300

    agora vai

--- pipeline_bronze/transformations/yfinance_bronze.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REFRESH STREAMING LIVE TABLE yfinance
-TBLPROPERTIES ("quality" = "bronze")
-AS
-SELECT *
-FROM cloud_files(
-  '/Volumes/lakehouse/raw_public/yfinance/commodities/latest_prices',
-  'json'
-);
